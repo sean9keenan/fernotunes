46cd4e4c3f1df10a698eb0679fd58e93fb88b11e
sean9keenan <sean9keenan@gmail.com>
2013-09-25 13:20:40 -0700

    MAc compat

--- Client/fernotunes.py
+++ Client/fernotunes.py
@@ -3,8 +3,58 @@ from twisted.internet import reactor
 from autobahn.websocket import WebSocketClientFactory, \
                                WebSocketClientProtocol, \
                                connectWS
+import os
+if not os.name == "posix":
+  from pytify import Spotify
+else:
+  import subprocess
 
-spotify = Spotify()
+
+app_name = "Spotify"
+
+def do_oscacript(command):
+  cmd = "osascript -e 'tell application \""+app_name+"\" to "+command+"'"
+  try:
+      return subprocess.check_output(cmd, shell=True)
+  except subprocess.CalledProcessError:
+      print "Failed to send message to "+app_name+", is it running?"
+      return False
+
+class MacVersion:
+  def playpause(self):
+    print "play/pause"
+    do_oscacript('playpause')
+
+  def previous(self):
+    do_oscacript('previous track')
+
+  def next(self):
+    do_oscacript('next track')
+
+  def volumeUp(self):
+    pass
+  def volumeDown(self):
+    pass
+  def isPlaying(self):
+    pass
+  def stop(self):
+    pass
+  def mute(self):
+    pass
+  def focus(self):
+    pass
+
+  def __str__(self):
+    artist = do_oscacript("artist of current track as string")
+    title = do_oscacript("name of current track as string")
+    album = do_oscacript("album of current track as string")
+    return title + "\n" + artist + " - " + album
+
+spotify = None
+if os.name == "posix":
+  spotify = MacVersion()
+else:
+  spotify = Spotify()
 
 allCommands = [spotify.playpause,
                spotify.previous,
@@ -16,7 +66,7 @@ allCommands = [spotify.playpause,
                spotify.mute,
                spotify.focus]
 
-def validCall(self, parameters):
+def validCall(parameters):
   return True
 
 def restartFactory():
@@ -35,10 +85,10 @@ class EchoClientProtocol(WebSocketClientProtocol):
     parameters = [x.strip() for x in msg.split(',')]
     try:
       if validCall(parameters):
-        allCommands[int(parameters[0])]
+        allCommands[int(parameters[0])]()
         self.sendMessage(str(spotify))
     except Exception, e:
-      print "Invalid Command passed"
+      print "Invalid Command passed" + str(e)
 
   def onClose(self, reason, r2, r3):
     print "Connection Closed: " + str(reason) + str(r2) + str(r3)
